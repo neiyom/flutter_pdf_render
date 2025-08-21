37f8bca3072aa8405358ab710606cc35086af737
Satish L. Suryavanshi <60255258+satishsuryavanshi1@users.noreply.github.com>
2025-08-21 18:35:43 +0530

    updated

---
+++ android/src/main/kotlin/jp/espresso3389/pdf_render/ByteBufferHelper.java
@@ -0,0 +1,13 @@
+package pub_pkg.flutter_pdf_render;
+
+import java.nio.ByteBuffer;
+
+class ByteBufferHelper {
+  static {
+    System.loadLibrary("bbhelper");
+  }
+
+  public static native ByteBuffer newDirectBuffer(long ptr, long size);
+  public static native long malloc(long size);
+  public static native void free(long ptr);
+}
